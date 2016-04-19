7aac0450e614122f624e6727d4cd0b6493fad4ad
Jason Warta <jasonwarta@gmail.com>
2016-04-19 12:24:05 -0800

    bootloader with hello world, colored rectangles

--- buildc.sh
+++ buildc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-gcc -c -g -Os -m32 -ffreestanding -Wall -Werror $1 -o test.o
-ld -static -melf_i386 -Ttest.ld -nostdlib --nmagic -o test.elf test.o
-objcopy -O binary test.elf test.bin
+rm test.o test.elf test.bin floppy.img 2> /dev/null
 
-dd if=/dev/zero of=floppy.img bs=512 count=2880
+gcc-4.4 -c -g -Os -m32 -ffreestanding -Wall -Werror $1 -o test.o &&
+ld -static -melf_i386 -Ttest.ld -nostdlib --nmagic -o test.elf test.o &&
+objcopy -O binary test.elf test.bin &&
+
+dd if=/dev/zero of=floppy.img bs=512 count=2880 &&
 dd if=test.bin of=floppy.img
